86c7985002c3e58c8b8aaef2bbc4869f020d97ca
Aleika <aleikaalves@hotmail.com>
2018-08-10 10:03:47 -0300

    Update nocandidato.cpp

--- nocandidato.cpp
+++ nocandidato.cpp
@@ -5,3 +5,16 @@ NoCandidato::NoCandidato(Candidato *_c, NoCandidato *_next)
     conteudo = _c;
     next = _next;
 }
+
+string NoCandidato::toString(){
+
+    NoCandidato *aux = this;
+    stringstream stream;
+
+    while(aux != NULL){
+        stream << aux->conteudo->sobrenome << " " << aux->conteudo->nome << " " << aux->conteudo->nota << " -> ";
+        aux = aux->next;
+    }
+    stream << "0";
+    return stream.str();
+}
